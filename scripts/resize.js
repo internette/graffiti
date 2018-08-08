ccbd2ace1d7869bafb899f9817c1827f918654ad
Antoinette Janus <acj.gfx@gmail.com>
2018-08-08 17:46:09 -0400

    Wrote resize script
    - renames gps2json call to appropriate name
    - wrote script that calls resize script
    - wrote resize script

---
+++ scripts/resize.js
@@ -0,0 +1,11 @@
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+const path = require('path');
+const output = path.join(__dirname, '../public/imgs/thumbnails');
+const originals = path.join(__dirname, '../public/imgs/full_width');
+
+const files = fs.readdirSync(originals);
+for(file of files){
+  const file_name = file.split('.');
+  execSync(`convert ${path.join(originals, file)} -resize 200 ${path.join(output, file_name[0])}_thumb.${file_name[1]}`);
+}
