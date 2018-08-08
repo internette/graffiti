7ba99cd3ba484e86100a3e25ec36d4e72184c884
Antoinette Janus <acj.gfx@gmail.com>
2018-08-08 17:51:41 -0400

    Resizing originals to be smaller in size (700px wide)

--- scripts/resize.js
+++ scripts/resize.js
@@ -7,5 +7,6 @@ const originals = path.join(__dirname, '../public/imgs/full_width');
 const files = fs.readdirSync(originals);
 for(file of files){
   const file_name = file.split('.');
+  execSync(`convert ${path.join(originals, file)} -resize 700 ${path.join(originals, file_name[0])}.${file_name[1]}`);
   execSync(`convert ${path.join(originals, file)} -resize 200 ${path.join(output, file_name[0])}_thumb.${file_name[1]}`);
 }
